8c5f6516041ab8411be9a3cc0fa67febcce95fd6
mukhammat <dosnet2200@gmail.com>
2025-09-09 23:59:42 +0300

    frontend

---
+++ crm-bakhodur-frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <router-view name="header"></router-view>
+  <RouterView />
+  <router-view name="footer"></router-view>
+</template>
