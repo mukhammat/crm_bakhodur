dc2d91a3aee325d8a32e30b0969014913285f8cc
mukhammat <dosnet2200@gmail.com>
2025-10-23 20:43:19 +0300

    qw

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ console.log('DATABASE_URL', process.env.DATABASE_URL);
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/database/schema',
-  out: './src/database/migrations',
+  out: './migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL || 'false',
   },
