8cdb8937e6684e2f4dd517b38fe0a2727c18a4de
mukhammat <dosnet2200@gmail.com>
2025-11-14 22:24:07 +0300

    fix redis

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -1,4 +1,4 @@
 import { Redis } from 'ioredis';
 
-const url = process.env.REDIS_URL ?? `redis://${process.env.REDIS_HOST ?? 'redis'}:${process.env.REDIS_PORT ?? 6379}`;
+const url = process.env.REDIS_URL ? `redis://${process.env.REDIS_HOST ?? 'redis'}:${process.env.REDIS_PORT ?? 6379}` : '';
 export const redis = new Redis(url);
