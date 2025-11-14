bf3434b7f09cf6017169f042b74fa2dfe83d59a5
Mukhammat <37116036+mukhammat@users.noreply.github.com>
2025-11-14 17:50:21 +0000

    qwe

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -1,2 +1,4 @@
 import { Redis } from 'ioredis';
-export const redis = new Redis();
+
+const url = process.env.REDIS_URL ?? `redis://${process.env.REDIS_HOST ?? 'redis'}:${process.env.REDIS_PORT ?? 6379}`;
+export const redis = new Redis(url);
